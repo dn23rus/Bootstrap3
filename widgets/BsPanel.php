199dc780e34f039171d923979457f45773f47d97
Francis Beresford <francis.beresford@gmail.com>
2014-02-25 09:59:39 +0800

    Allow a footer to be added in a BsPanel

--- widgets/BsPanel.php
+++ widgets/BsPanel.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * BsPortlet class file.
+ * BsPanel class file.
  * @author Francis Beresford <francis.beresford@gmail.com>
  * @license http://opensource.org/licenses/MIT The MIT License
  * @package bootstrap/widgets
@@ -30,16 +30,56 @@ class BsPanel extends CPortlet
      * @var string the CSS class for the portlet title tag. Defaults to 'panel-title'.
      */
     public $titleCssClass = 'panel-title';
+    /**
+     * @var string the footer of the portlet. Defaults to null.
+     * When this is not set, the footer will not be displayed.
+     * Note that the footer will not be HTML-encoded when rendering.
+     */
+    public $footer;
+    /**
+     * @var string the CSS class for the panel footer tag. Defaults to 'panel-title'.
+     */
+    public $footerCssClass = 'panel-footer';
 
     /**
      * @var string the CSS class which defines the Panel's context http://getbootstrap.com/components/#panels-alternatives.
      */
     public $type = BSHtml::PANEL_TYPE_DEFAULT;
 
+    private $_openTag;
+
     public function init()
     {
         $this->htmlOptions = BSHtml::addClassName($this->type, $this->htmlOptions);
-        parent::init();
+        ob_start();
+        ob_implicit_flush(false);
+
+        if(isset($this->htmlOptions['id']))
+            $this->id=$this->htmlOptions['id'];
+        else
+            $this->htmlOptions['id']=$this->id;
+        echo CHtml::openTag($this->tagName,$this->htmlOptions)."\n";
+        $this->renderDecoration();
+        echo "<div class=\"{$this->contentCssClass}\">\n";
+
+        $this->_openTag=ob_get_contents();
+        ob_clean();
+    }
+
+    /**
+     * Renders the content of the portlet.
+     */
+    public function run()
+    {
+        $this->renderContent();
+        $content=ob_get_clean();
+        if($this->hideOnEmpty && trim($content)==='')
+            return;
+        echo $this->_openTag;
+        echo $content;
+        echo "</div>\n";
+        echo $this->renderFooter();
+        echo CHtml::closeTag($this->tagName);
     }
 
     /**
@@ -51,6 +91,19 @@ class BsPanel extends CPortlet
         if ($this->title !== null) {
             echo "<div class=\"{$this->decorationCssClass}\">\n";
             echo "<h3 class=\"{$this->titleCssClass}\">{$this->title}</h3>\n";
+            echo "</div>\n";
+        }
+    }
+
+    /**
+     * Renders the decoration for the portlet.
+     * The default implementation will render the title if it is set.
+     */
+    protected function renderFooter()
+    {
+        if ($this->footer !== null) {
+            echo "<div class=\"{$this->footerCssClass}\">\n";
+            echo $this->footer."\n";
             echo "</div>\n";
         }
     }
