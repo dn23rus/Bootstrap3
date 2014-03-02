f6d26c41cba96d4b2bb7643301668482bbb523db
DrMabuse <pascalbrewing@googlemail.com>
2014-03-02 16:13:51 +0100

    remove sass task for extensions

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,32 +27,6 @@ module.exports = function (grunt) {
                     }
                 ]
             }
-        },
-        sass: {                              // Task
-            ibuttondist: {                            // Target
-                options: {                       // Target options
-                    style: 'compact'
-                },
-                files: {                         // Dictionary of files
-                    'extensions/ibutton/resources/css/jquery.ibutton.css': extIbuttonAsset + 'jquery.ibutton.scss'       // 'destination': 'source'
-                }
-            },
-            ibuttonprod: {
-                options: {                       // Target options
-                    style: 'compressed'
-                },
-                files: {                         // Dictionary of files
-                    'extensions/ibutton/resources/css/jquery.ibutton.min.css': extIbuttonAsset + 'jquery.ibutton.scss'       // 'destination': 'source'
-                }
-            }
-        },
-        watch: {
-            ibuttoncss: {
-                files: extIbuttonAsset + 'jquery.ibutton.scss',
-                tasks: ['sass:ibuttondist', 'sass:ibuttonprod']
-
-            }
-
         }
     });
 
